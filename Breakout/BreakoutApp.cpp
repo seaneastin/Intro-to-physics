b873b24667242e6824da5a5fe3b8d2967f19e462
Sean Eastin <sean.eastin@gamil.com>
2020-08-18 11:00:15 -0500

    created, upgraded, and set up Breakout project

---
+++ Breakout/BreakoutApp.cpp
@@ -0,0 +1,61 @@
+#include "BreakoutApp.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+#include <glm\ext.hpp>
+#include <Gizmos.h>
+
+BreakoutApp::BreakoutApp() {
+
+}
+
+BreakoutApp::~BreakoutApp() {
+
+}
+
+bool BreakoutApp::startup() {
+	
+	//Initialize the Gizmos singleton class
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
+	m_2dRenderer = new aie::Renderer2D();
+
+	// TODO: remember to change this when redistributing a build!
+	// the following path would be used instead: "./font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+	return true;
+}
+
+void BreakoutApp::shutdown() {
+
+	delete m_font;
+	delete m_2dRenderer;
+}
+
+void BreakoutApp::update(float deltaTime) {
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+}
+
+void BreakoutApp::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	// draw your stuff here!
+	static float aspectRatio = 16 / 9.f;
+	aie::Gizmos::draw2D(glm::ortho<float>(-100, 100, -100 / aspectRatio, 100 / aspectRatio , -1.0f, 1.0f));
+	// output some text, uses the last used colour
+	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
