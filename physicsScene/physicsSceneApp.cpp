7df000ebd95d07ec9f6d7de7d08aec988e75aa68
Sean Eastin <sean.eastin@gamil.com>
2020-08-18 15:02:07 -0500

    created and upgraded PhysicsScene project

---
+++ physicsScene/physicsSceneApp.cpp
@@ -0,0 +1,56 @@
+#include "physicsSceneApp.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+
+physicsSceneApp::physicsSceneApp() {
+
+}
+
+physicsSceneApp::~physicsSceneApp() {
+
+}
+
+bool physicsSceneApp::startup() {
+	
+	m_2dRenderer = new aie::Renderer2D();
+
+	// TODO: remember to change this when redistributing a build!
+	// the following path would be used instead: "./font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+	return true;
+}
+
+void physicsSceneApp::shutdown() {
+
+	delete m_font;
+	delete m_2dRenderer;
+}
+
+void physicsSceneApp::update(float deltaTime) {
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+}
+
+void physicsSceneApp::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	// draw your stuff here!
+	
+	// output some text, uses the last used colour
+	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
